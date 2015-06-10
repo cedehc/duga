594ef9c2a278e01d3c74a473c1c858b10ddf5fdc
Igor Almeida <igor.contato@gmail.com>
2015-06-10 13:51:42 -0300

    Separar arquivos e adicionar licença

--- api/memory_keeper.go
+++ api/memory_keeper.go
@@ -16,40 +16,27 @@
 */
 package api
 
-import (
-	"github.com/coocood/jas"
-)
+type FakeDb []Comic
 
-type ComicId struct {
-	ComicKeeper Keeper
+func NewFakeDb() *FakeDb {
+	var tmp FakeDb = make([]Comic, 0, 10)
+
+	return &tmp
 }
 
-type Comic struct {
-	Title string
-	Link  string
+func (p *FakeDb) FakeData() {
+	*p = append(*p, Comic{"xkcd", "xkcd.com"})
+	*p = append(*p, Comic{"abstrusegoose", "abstrusegoose.com"})
 }
 
-func (p *ComicId) Get(ctx *jas.Context) {
-	id := int(ctx.Id)
-
-	tmp, err := p.ComicKeeper.Fetch(id)
-
-	if err == nil {
-		ctx.Data = tmp
-	} else {
-		ctx.Error = jas.NewRequestError(err.Error())
+func (p *FakeDb) Fetch(id int) (Comic, error) {
+	if id < p.Count() {
+		return (*p)[id], nil
 	}
+
+	return Comic{}, &ComicError{"invalid id"}
 }
 
-type ComicError struct {
-	Msg string
-}
-
-func (e *ComicError) Error() string {
-	return e.Msg
-}
-
-type Keeper interface {
-	Count() int
-	Fetch(id int) (Comic, error)
+func (p *FakeDb) Count() int {
+	return len(*p)
 }
